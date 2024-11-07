c2ca6812049c84c04f3d4726bdc20e8ce9e6e2e1
Joovs <jovannaescogar9@gmail.com>
2024-11-07 16:55:01 -0600

    API login tambien se instalaron dependecias y se actualizaron los requerimientos

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,10 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import pymongo
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 # Habilitar CORS para permitir solicitudes desde React Native
 CORS(app)
@@ -28,7 +29,7 @@ try:
 
     # el ping es un mensaje que se manda a la bd para saber si hay conexión
     db.command("ping")
-    print("Connected successfully")
+    print("Connected successfully bby")
 except Exception as e:
     raise Exception(
         "The following error occurred: ", e)
@@ -53,7 +54,6 @@ def get_users():
 # API REGISTRAR USUARIO
 @app.route('/userRegistration', methods=['POST'])
 def registrar_usuario():
-
     #esto es para obtener el cuerpo de la solicitud
     data = request.get_json()
 
@@ -67,9 +67,7 @@ def registrar_usuario():
         print('El usuario ya existe, bb')
         return jsonify({'error': 'El usuario ya está registrado'}), 400
     
-
     try:
-
         nuevo_usuario = {
         'nombre': data['name'],
         'email': data['mail'],
@@ -77,7 +75,6 @@ def registrar_usuario():
         'contrasena': generate_password_hash(data['passw'])  # Encriptar la contraseña
         #'contrasena': data['passw']
         }
-
         # Insertar el usuario en la base de datos
         collection_users.insert_one(nuevo_usuario)
         print('Ya se registró')
@@ -87,6 +84,57 @@ def registrar_usuario():
         print("Error al obtener registrar el usuario:", str(e))
         return jsonify({"error": "Ocurrió un error al hacer el registro"}), 500 
 
+
+# FUNCIÓN PARA VALIDAR EL USUARIO Y CONTRASEÑA
+def validar_usuario(mail, passw):
+    
+    #email = collection_users.find_one({"email": mail})
+    password = collection_users.find_one({"email": mail}, {"contrasena": 1, '_id': 0})
+    if password:
+        dato = password.get('contrasena')
+    print(dato)
+
+    #if dato==passw:
+    #if bcrypt.checkpw(passw.encode('utf-8'), dato):
+    if check_password_hash(dato, passw):
+        print('Contraseña correcta, bb')
+        return True
+    else:
+        print('Contraseña No es correcta, bb')
+        return False
+
+   
+# API LOGIN USUARIO
+@app.route('/userValidation', methods=['POST'])
+def login ():
+    # datos requeridos para hacer la petición o validación
+    data = request.json
+
+    # Validar si se proporcionan todos los campos requeridos
+    if not data or not data.get('mail') or not data.get('passw'):
+        print('Faltan datos bb')
+        return jsonify({'error': 'Faltan datos requeridos bby'}), 400
+    
+    print(data.get('mail'))
+    print(data.get('passw'))
+    print("--------")
+    username = data.get('mail')
+    password = data.get('passw')
+
+    #validar_usuario(username, password)
+    
+    #return jsonify({"parametros obtenidos": "{data.get('mail')}"})
+
+    if validar_usuario(username, password):
+        print("Login exitoso")
+        return jsonify({"message": "Login exitoso"}), 200
+    else:
+        return jsonify({"error": "Usuario o contraseña incorrectos"}), 401
+   
+
+        
+
+    
 
 
 
